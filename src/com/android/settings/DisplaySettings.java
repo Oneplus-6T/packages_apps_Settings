6fee1a0c7d902759911201d855a7493743911886
Shikhar <krtonia@gmail.com>
2026-01-09 08:48:52 +0000

    Settings: Drop IconPack customisations
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -98,8 +98,6 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.icon_pack"));
         return controllers;
     }
 
